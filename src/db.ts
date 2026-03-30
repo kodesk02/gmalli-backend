d8b597019ea2346be524a6f182c589ec3e8a828b
macbook <macbook@Kitans-Pro.local>
2026-03-30 12:34:58 +0100

    final fix

--- src/db.ts
+++ src/db.ts
@@ -1,20 +1,20 @@
 import { Pool } from "pg";
 
-const pool = new Pool ({
-  user: "macbook",
-  host: "localhost",
-  database: "marketplace",
-  password: "",
-  port: 5432,
-})
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+});
 
 pool.connect((err, client, release) => {
-    if(err){
-        console.log('Database failed due to:', err.message)
-    } else {
-        console.log('Connected to postgressSQl')
-        release()
-    }
-})
+  if (err) {
+    console.log("Database failed due to:", err.message);
+  } else {
+    console.log("Connected to postgressSQl");
+    release();
+  }
+});
 
 export default pool;
